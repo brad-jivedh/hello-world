9c8537cb55a8bb60ed5fb99dd4faddc9833e22c7
brad-jivedh <163302259+brad-jivedh@users.noreply.github.com>
2024-04-13 14:05:56 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,16 +1,16 @@
 node {
   agent any
-   stage('Checkout stage') {
-       sh 'mvn --version'
+      stage('Checkout stage') {
+        sh 'mvn --version'
      }
-  stage('Build stage') {
-       sh 'mvn clean install'
+       stage('Build stage') {
+        sh 'mvn clean install'
      }
-  stage('Push stage') {
-      echo "Pushin the build to tomcat"
+       stage('Push stage') {
+        echo "Pushin the build to tomcat"
+     }
+       stage('Deploy stage') {
+         sh 'sudo mv /var/lib/jenkins/workspace/Que_4B/target/8.war /opt/apache-tomcat-9.0.87/webapps/'
   }
-  stage('Deploy stage') {
-    sh 'sudo mv /var/lib/jenkins/workspace/Que_4B/target/8.war /opt/apache-tomcat-9.0.87/webapps/'
   }
-   }
 
