fd16765691a7ae4f38c576bed9bc59fc34d60655
brad-jivedh <163302259+brad-jivedh@users.noreply.github.com>
2024-04-13 14:18:21 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -11,6 +11,6 @@ node {
         echo 'This is Push Stage'
     }
     stage('deploy') {
-        sh 'sudo mv /var/lib/jenkins/workspace/Que_4B/target/8.war /opt/apache-tomcat-9.0.87/webapps/'
+        sh 'sudo mv /var/lib/jenkins/workspace/Que_4B/target/*.war /opt/apache-tomcat-9.0.87/webapps/'
     }
 }
